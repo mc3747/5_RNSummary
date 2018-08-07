43401c39eaa79ff66ffcc5545701c45c8d3112f9
mc3747 <mc3747@126.com>
2018-08-07 14:11:11 +0800

    180807-添加全局注释

--- src/0_Resources/Common.js
+++ src/0_Resources/Common.js
@@ -1,5 +1,23 @@
-
+/**
+ * @providesModule Common
+ */
 import {
     Dimensions
 } from 'react-native';
-let {height,width} = Dimensions.get('window');
+
+
+/* 
+@providesModule使用
+
+在文件的顶部，嵌套一个多行注释，把@providesModule放在注释里,@providesModule后添加类名,以后就直接使用类名就能导入了。
+
+注意点，带有@providesModule的多行注释，一定要是文件的第一个多行注释。
+*/
+export default class Common {
+
+    static bgColor = 'rgb(232,232,232)';
+
+    static screenW = Dimensions.get('window').width;
+
+    static screenH = Dimensions.get('window').height;
+}
